707f270f2ca2f5531c3b8f094914c5d86986b061
Rodrigo Figueira <rodrigo.figueiraufg@gmail.com>
2020-08-03 11:24:03 -0300

    Alteração da estrutura e inclusão do projeto de test.

---
+++ src/PollIO.Test/PollControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using PollIO.Api.Controllers;
+using PollIO.Api.ViewModels;
+using PollIO.Business.Interfaces;
+using PollIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PollIO.Test
+{
+    public class PollControllerTest
+    {
+        PollController _controller;
+        IPollService _service;
+        private readonly IPollRepository _pollRepository;
+        private Mock _mapper;
+        INotificador notificador;
+
+        public PollControllerTest()
+        {
+            _service = new PollServiceFake();
+            //_mapper = new Mock<IMapper>();
+            //_controller = new PollController(_pollRepository, _mapper, _service,notificador);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsOkResult()
+        {
+            //Arrange
+            var poll = new PollDto
+            {
+                Id = 7,
+                Poll_description = "Testando poll 7",
+                Options = new List<OptionPollDto>
+                {
+                    new OptionPollDto
+                    {
+                        Description = "Opção 7",
+                        Id = 3,
+                        Votes = "3"
+                    }
+                }
+            };
+
+            // Act
+            var okResult = _controller.Adicionar(poll);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+    }
+}
---
+++ src/PollIO.Test/PollServiceFake.cs
@@ -0,0 +1,95 @@
+﻿using PollIO.Business.Interfaces;
+using PollIO.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PollIO.Test
+{
+    class PollServiceFake : IPollService
+    {
+        private readonly IList<Business.Models.Poll> _poll;
+
+        public PollServiceFake()
+        {
+            _poll = new List<Business.Models.Poll>()
+            {
+                new Business.Models.Poll
+                {
+                    Id = 1,
+                    Poll_description = "Testando 1",
+                    Views = "2",
+                    Options = new List<OptionsPoll>
+                    {
+                        new OptionsPoll
+                        {
+                            Description = "Opção 1",
+                            Id = 1,
+                            PollId = 1
+                        }
+                    }
+                },
+                new Business.Models.Poll
+                {
+                    Id = 2,
+                    Poll_description = "Testando 2",
+                    Views = "4",
+                    Options = new List<OptionsPoll>
+                    {
+                        new OptionsPoll
+                        {
+                            Description = "Opção 2",
+                            Id = 2,
+                            PollId = 2
+                        }
+                    }
+                },
+                new Business.Models.Poll
+                {
+                    Id = 3,
+                    Poll_description = "Testando 3",
+                    Views = "6",
+                    Options = new List<OptionsPoll>
+                    {
+                        new OptionsPoll
+                        {
+                            Description = "Opção 3",
+                            Id = 3,
+                            PollId = 3
+                        }
+                    }
+                }
+            };
+        }
+
+        public async Task<Business.Models.Poll> Adicionar(Business.Models.Poll pollParaAdd)
+        {
+            _poll.Add(pollParaAdd);
+            return pollParaAdd;
+        }
+
+        public Task<bool> Atualizar(Business.Models.Poll fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Remover(int id)
+        {
+            var item = _poll.First(p => p.Id == id);
+            _poll.Remove(item);
+            return true;
+        }
+
+        public Task<bool> Vote(int id, OptionsPoll option)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
